c861992f5e28ef0cd4a68e8f5351177d9a43f46a
AkirichBoroda <DPAKOH90@mail.ru>
2024-10-08 22:16:01 +0300

    init

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+    static string inputArr()
+    {
+        // input array as string
+        Console.WriteLine("Введите строки через пробел:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+    }
+    static int searchShortString ()
+    {
+
+    }
+}
