b765be1d94f8624d4316a15abb95fbb5f880be2a
AkirichBoroda <DPAKOH90@mail.ru>
2024-10-09 00:09:28 +0300

    final

--- Program.cs
+++ Program.cs
@@ -1,56 +1,21 @@
-﻿using System;
-using System.Reflection.Metadata.Ecma335;
-
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         // Вводим исходный массив строк с клавиатуры или задаем его программно
-//         Console.WriteLine("Введите элементы массива через пробел:");
-//         string input = Console.ReadLine();
-//         string[] initialArray = input.Split(' ');
-
-//         // Считаем количество строк, длина которых меньше или равна 3 символам
-//         int count = 0;
-//         for (int i = 0; i < initialArray.Length; i++)
-//         {
-//             if (initialArray[i].Length <= 3)
-//             {
-//                 count++;
-//             }
-//         }
-
-//         // Создаем новый массив нужного размера
-//         string[] resultArray = new string[count];
-
-//         // Заполняем новый массив
-//         int index = 0;
-//         for (int i = 0; i < initialArray.Length; i++)
-//         {
-//             if (initialArray[i].Length <= 3)
-//             {
-//                 resultArray[index] = initialArray[i];
-//                 index++;
-//             }
-//         }
-
-//         // Вывод массива в строку через запятую
-//         Console.WriteLine("Результат: ");
-//         Console.WriteLine(string.Join(", ", resultArray));
-//     }
-// }
-
+﻿
 class Program
 {
     static void Main()
     {
         string[] tmp = new string[1] {"0"};
         string[] inputArray = inputArr();
-        // int localtmp = Convert.ToInt16(passingArray(inputArr(),false, tmp));
-        // Console.WriteLine(string.Join(", ",passingArray(inputArr(),false, tmp)));
         string[] resultArray = new string[Convert.ToInt16(passingArray(inputArray,false, tmp)[0])];
-        resultArray = passingArray(inputArray,true, resultArray);
-        Console.WriteLine(string.Join(", ", resultArray));
+        if (resultArray.Length > 0)
+        {
+            resultArray = passingArray(inputArray,true, resultArray);
+            Console.WriteLine(string.Join(", ", resultArray));
+        }
+        else
+        {
+            Console.WriteLine("отсутствуют строки подходящие под условие");
+        }
+        
 
     }
     static string[] inputArr()
