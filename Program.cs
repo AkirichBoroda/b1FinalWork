730d073ee925fdfd7bc5a34bbcbf07fa9ec0e81e
AkirichBoroda <DPAKOH90@mail.ru>
2024-10-08 23:47:55 +0300

    add passingArray

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿﻿using System;
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 
 
@@ -6,17 +7,48 @@ class Program
 {
     static void Main()
     {
+        string[] tmp = new string[1] {"0"};
+        // int localtmp = Convert.ToInt16(passingArray(inputArr(),false, tmp));
+        Console.WriteLine(string.Join(", ",passingArray(inputArr(),false, tmp)));
+        // string[] resultArray = new string[Convert.ToInt16(passingArray(inputArr(),false, tmp))];
+        // Console.WriteLine(string.Join(", ", resultArray));
 
     }
-    static string inputArr()
+    static string[] inputArr()
     {
         // input array as string
         Console.WriteLine("Введите строки через пробел:");
         string input = Console.ReadLine();
         string[] inputArray = input.Split(' ');
+        return inputArray;
     }
-    static int searchShortString ()
+    static string[] passingArray(string[] arr, bool flag, string[] resultArray)
     {
-
+        int ctr = 0;
+        string[] stringCtr = new string[1];
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3)
+            {
+                if (flag)
+                {
+                    resultArray[ctr] = arr[i];
+                    ctr++;
+                }
+                else
+                {
+                    ctr++;
+                }
+                
+            }
+        }
+        
+        if (flag) return resultArray;
+        stringCtr[0] = Convert.ToString(ctr);
+        return stringCtr;
+        
+        
     }
+    
 }
